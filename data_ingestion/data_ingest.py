fe407fded9392c6021657e6f926416ef49fdba9c
shashi <shashipolity@gmail.com>
2025-04-26 21:20:48 +0530

    changes

--- data_ingestion/data_ingest.py
+++ data_ingestion/data_ingest.py
@@ -1,9 +1,18 @@
-import pandas as pd
-from langchain_astradb import AstraDBGraphVectorStore
+from langchain_astradb import AstraDBVectorStore
 from dotenv import load_dotenv
 import os
+import pandas as pd
+from data_ingestion.data_transform import data_conveter
 
 
 # data = pd.read_csv("data/amazon_product_review.csv")
 
 # print(data.shape)
+
+
+class ingest_data:
+    def __init__(self):
+        pass
+    
+    def data_ingestion(self):
+        pass
